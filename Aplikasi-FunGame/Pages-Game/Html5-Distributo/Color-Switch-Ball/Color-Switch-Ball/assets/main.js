6bcdf5127cb805640facaf7a0a5837edb95a0733
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-03-30 09:42:51 +0700

    first commit

---
+++ Aplikasi-FunGame/Pages-Game/Html5-Distributo/Color-Switch-Ball/Color-Switch-Ball/assets/main.js
@@ -0,0 +1,16 @@
+function getParentUrl() {
+    var url = (window.location !== window.parent.location)
+        ? (document.referrer && document.referrer !== '')
+            ? document.referrer
+            : document.location.href
+        : document.location.href;
+    if (document.referrer.indexOf('https://www.fungames.my.id/') !== -1) {
+        url = 'https://gamedistribution.com/';
+    }
+    return url;
+}
+// Add the self-hosted game url as value of gameUrl.
+var gameUrl = 'https://html5.gamedistribution.com/50d9086380f1469b8d17df9e3b64d078/';
+document
+    .getElementById('FunGames')
+    .src = gameUrl + '?gd_sdk_referrer_url=' + getParentUrl();
