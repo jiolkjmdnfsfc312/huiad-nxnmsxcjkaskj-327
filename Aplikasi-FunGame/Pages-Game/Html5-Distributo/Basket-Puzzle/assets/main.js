bb816e925d2cd06dde60cbca4f75c8b55bb3cf18
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-04-05 15:23:47 +0700

    first commit

--- Aplikasi-FunGame/Pages-Game/Html5-Distributo/Basket-Puzzle/assets/main.js
+++
@@ -1,2 +0,0 @@
-
-    document.getElementById("FunGames").src ="https://html5.gamedistribution.com/01a28589cb4c4b949ea08c8de26a8ee7/";
---
+++ Aplikasi-FunGame/Pages-Game/Html5-Distributo/Drive-Taxi/Drive-Taxi/assets/main.js
@@ -0,0 +1,16 @@
+function getParentUrl() {
+    var url = (window.location !== window.parent.location)
+        ? (document.referrer && document.referrer !== '')
+            ? document.referrer
+            : document.location.href
+        : document.location.href;
+    if (document.referrer.indexOf('https://www.fungames.my.id/') !== -1) {
+        url = 'https://gamedistribution.com/';
+    }
+    return url;
+}
+// Add the self-hosted game url as value of gameUrl.
+var gameUrl = 'https://html5.gamedistribution.com/b6e11aee3ba64b48be2bda6b23bf4bee/';
+document
+    .getElementById('FunGames')
+    .src = gameUrl + '?gd_sdk_referrer_url=' + getParentUrl();
