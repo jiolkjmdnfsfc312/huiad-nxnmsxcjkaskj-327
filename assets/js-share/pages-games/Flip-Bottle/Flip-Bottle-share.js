80e142e62c0336de55bf8898f5e2a216e9554a65
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-04-09 13:08:44 +0700

    first commit

---
+++ assets/js-share/pages-games/Flip-Bottle/Flip-Bottle-share.js
@@ -0,0 +1,39 @@
+// share wa
+var element = document.getElementById("Whatsapp");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://wa.me/?text=Flip Bottle FunGames : https://www.fungames.my.id/Pages-Games/Flip-Bottle/';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// share wa
+
+// share telegram
+var element = document.getElementById("Telegram");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://telegram.me/share/url?text=Flip Bottle FunGames : https://www.fungames.my.id/Pages-Games/Flip-Bottle/';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// share telegram
+
+/* Gmail *
+var element = document.getElementById("Gmail");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://mail.google.com/mail/?view=cm&to=%7Bemail_address%7D&su=Download%20Design%20BCA%20Mobile%20%7C%20Stack%20Image&body=https://www.stackimage.my.id/File/Download-Design/Design-Bca-Mobile/&bcc=%7Bemail_address%7D&cc=%7Bemail_address%7D';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+/* Gmail */
+
+copy.onclick = () => {
+  input.select(); //select input value
+  if (document.execCommand("copy")) { //if the selected text copy
+    field.classList.add("active");
+    copy.innerText = "Copied";
+    setTimeout(() => {
+      window.getSelection().removeAllRanges(); //remove selection from document
+      field.classList.remove("active");
+      copy.innerText = "Copy";
+    }, 3000);
+  }
+}
--- assets/js/fungames-elm.js
+++ assets/js/fungames-elm.js
@@ -11,6 +11,12 @@ document.onkeydown = function (e) {
     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
         return false;
     }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'P'.charCodeAt(0)) {
+        return false;
+    }
     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
         return false;
     }
