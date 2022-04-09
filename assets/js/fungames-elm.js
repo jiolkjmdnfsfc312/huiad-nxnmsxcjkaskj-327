9df1d364914b92207e10820a6643336e1af3163b
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-04-09 23:29:09 +0700

    first commit

--- assets/js/fungames-elm.js
+++ assets/js/fungames-elm.js
@@ -36,3 +36,21 @@ document.onkeydown = function (e) {
         return false;
     }
 }
+
+
+(function () {
+    if (!$('body').hasClass('debug_mode')) {
+        var _z = console;
+        Object.defineProperty(window, "console", {
+            get: function () {
+                if ((window && window._z && window._z._commandLineAPI) || {}) {
+                    throw "Nice trick! but not permitted!";
+                }
+                return _z;
+            },
+            set: function (val) {
+                _z = val;
+            }
+        });
+    }
+})();
