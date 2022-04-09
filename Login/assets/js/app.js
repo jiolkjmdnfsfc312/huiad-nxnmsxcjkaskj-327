5545da3f28a7aa452324455c8e5d33a64bb187c1
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-04-10 00:13:14 +0700

    first commit

--- Login/assets/js/app.js
+++ Login/assets/js/app.js
@@ -18,3 +18,24 @@ $(".textbox input").focusout(function(){
       $(".login-btn").removeClass("active");
     }
   });
+
+
+  
+
+  const toastContainer = document.querySelector(".toast-container");
+const closeBtn = document.querySelector(".toast-container .close");
+const toastLink = document.querySelector(".toast-container a");
+
+if (!localStorage.getItem("displayToast")) {
+  setTimeout(() => {
+    toastContainer.classList.add("active");
+  }, 1000);
+}
+
+const stopDisplayingToast = () => {
+  
+  toastContainer.classList.remove("active");
+};
+
+closeBtn.addEventListener("click", stopDisplayingToast);
+toastLink.addEventListener("click", stopDisplayingToast);
